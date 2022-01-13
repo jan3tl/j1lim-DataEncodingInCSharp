9216c927b43ca6d9c174172835a9619652656d02
jan3tl <j1lim2023@chadwickschool.org>
2022-01-13 11:50:16 -0800

    added files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace j1lim_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
