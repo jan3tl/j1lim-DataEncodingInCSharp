172169e0190401ffb02186f961ba6b9ee64bd328
jan3tl <j1lim2023@chadwickschool.org>
2022-01-13 21:39:54 -0800

    chore: finished most of first activity

--- Program.cs
+++ Program.cs
@@ -15,7 +15,48 @@ namespace j1lim_DataEncodingInCSharp
             Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
 
             int toConvert;
-            
+            Console.Write("Enter an integer: ");
+            toConvert = int.Parse(Console.ReadLine());
+
+            char asChar;
+            asChar = (char)toConvert;
+            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+
+            char b;
+            b = (char)('A' + 1);
+            Console.WriteLine($"'A' + 1 = {b}");
+
+            char x;
+            x = (char)('Z' + 3 );
+            Console.WriteLine($"'Z' + 3 = {x}");
+
+            char y;
+            y = (char)('F' - 5);
+            Console.WriteLine($"'F' - 5 = {y}");
+
+            char f;
+            f = (char)('K' - 3);
+            char g;
+            g = (char)('h' - 3);
+            char h;
+            h = (char)('o' - 3);
+            char i;
+            i = (char)('r' - 3);
+            char j;
+            j = (char)('#' - 3);
+            char k;
+            k = (char)('Z' - 3);
+            char m;
+            m = (char)('u' - 3);
+            char n;
+            n = (char)('g' - 3);
+            char o;
+            o = (char)('$' - 3);
+            Console.WriteLine("What is the result of shifting each character in the string 'Khoor#Zruog$' by -3?");
+            Console.WriteLine($"'{f}{g}{h}{h}{i}{j}{k}{i}{m}{h}{n}{o}'");
+
+
+
         }
     }
 }
